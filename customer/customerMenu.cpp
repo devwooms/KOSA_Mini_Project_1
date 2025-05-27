2be83875ea64780f43924e431c2928acbd364e0c
devwooms <devwooms@gmail.com>
2025-05-27 13:43:43 +0900

    update

---
+++ customer/customerMenu.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+void customerMenu(){
+    std::cout << "1. 제품 조회" << std::endl;
+    std::cout << "2. 제품 구매" << std::endl;
+    std::cout << "3. 제품 장바구니" << std::endl;
+    std::cout << "4. 제품 구매 내역" << std::endl;
+    std::cout << "5. 제품 후기" << std::endl;
+    std::cout << "6. 제품 후기 내역" << std::endl;
+}
